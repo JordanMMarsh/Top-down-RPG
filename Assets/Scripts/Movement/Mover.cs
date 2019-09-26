833833d64498eb9a493a37146b6db81f35eba853
Jordan Marsh <jordanmarshdesign@gmail.com>
2019-09-26 11:21:46 -0700

    Navmesh, camera follows player, animation and blend tree

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    NavMeshAgent navMeshAgent;
+    Animator animator;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            SetDestination();
+        }
+        UpdateAnimator();
+    }
+
+    private void SetDestination()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            navMeshAgent.SetDestination(hit.point);
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = navMeshAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        animator.SetFloat("forwardSpeed", speed);
+    }
+}
